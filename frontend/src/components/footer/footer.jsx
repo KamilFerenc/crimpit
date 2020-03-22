4093df16d096ea8a0e0bd30125aa54158eb52b5c
berbua <katarzyna.berbeka@creativestyle.pl>
2020-03-22 16:44:42 +0100

    Add landing page, with nav and footer, sign in page

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './footer.scss';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer class="footer mt-auto py-3 bg-light">
+                <div class="container">
+                    <span class="text-muted">Copiright: The Fernenz</span>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ frontend/src/components/landingPage/landingPage.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './landingPage.scss';
+import crimpImage from '../../images/crimp.png';
+import measureImage from '../../images/measure.png';
+import progressImage from '../../images/progress.png';
+
+class LandingPage extends Component {
+    state = {};
+    render() {
+        return (
+            <main className="ci-landing-page__wrapper">
+                <div className="container ci-landing-page__grid">
+                    <div className="row ci-landing-page__logo-wrapper">
+                        <div className=" ci-landing-page__logo">
+                            <h4>Crimp it!</h4>
+                        </div>
+                    </div>
+                    <div className="row ci-landing-page__features">
+                        <div className="col ci-landing-page__feature">
+                            <h3 className="ci-landing-page__feature-title">
+                                Test
+                            </h3>
+                            <img
+                                src={crimpImage}
+                                className="ci-landing-page__feature-image"
+                            ></img>
+                        </div>
+                        <div className="col ci-landing-page__feature">
+                            <h3 className="ci-landing-page__feature-title">
+                                Measure
+                            </h3>
+                            <img
+                                src={measureImage}
+                                className="ci-landing-page__feature-image"
+                            ></img>
+                        </div>
+                        <div className="col ci-landing-page__feature">
+                            <h3 className="ci-landing-page__feature-title">
+                                Progress
+                            </h3>
+                            <img
+                                src={progressImage}
+                                className="ci-landing-page__feature-image"
+                            ></img>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col ci-landing-page__features-signup">
+                            <button
+                                className="btn btn-outline-primary ci-landing-navigation__login-button m-2"
+                                type="button"
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        );
+    }
+}
+
+export default LandingPage;
